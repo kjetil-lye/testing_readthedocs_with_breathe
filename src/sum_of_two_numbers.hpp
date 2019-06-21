d64ad3c7e224b36bb7f748dc69b812569bb5a9d1
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-06-21 09:12:28 +0200

    added first commit

---
+++ src/sum_of_two_numbers.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+//! Computes the sum of two numbers
+//!
+//! @param a the first operand
+//! @param b the second operand
+//! @return \f$a+b\f$
+//!
+//! @note this is quite notable.
+template<class T>
+T sum_of_two_numbers(const T& a, const T& b) {
+  return a + b;
+}
